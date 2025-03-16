c765c0de9d695219734b852de25e761a375a7219
Julia Marushchenko <marushchenko.yulia@gmail.com>
2025-03-17 00:08:52 +0100

    Add files via upload
    Commiting class Main

---
+++ src/com/operators/Main.java
@@ -0,0 +1,21 @@
+package com.operators;
+
+// Class
+public class Main {
+
+    // Method main to run java application
+    public static void main(String[] args) {
+
+        // Local variables
+        int variable = 6;
+        int variable1 = 7;
+
+        // Concatenation string with +
+        System.out.println("Concatenation variable and variable1 " + 6 + 7); // Prints Concatenation variable
+        // and variable1 67
+
+        // Adding variable and variable1
+        System.out.println("Concatenation variable and variable1 " + (6 + 7)); // Prints Concatenation variable
+        // and variable1 13
+    }
+}
